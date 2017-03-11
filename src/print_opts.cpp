cd69aa11cebea080017ebc7b71da50a6d720639e
RebeccaMay <remay@mines.edu>
2017-03-10 21:01:07 -0500

    added print opts

---
+++ src/print_opts.cpp
@@ -0,0 +1,15 @@
+#include "print_opts.h"
+
+void Print_opts::print_state_transition(const Event* event, Thread::State before_state, Thread::State after_state) {
+}
+
+
+void Print_opts::print_dispatch_invoked_message(const Event* event, Thread *thread, std::string message) {
+}
+
+void Print_opts::print_process_details(Process* process) const {
+
+}
+
+//void Print_opts::print_statistics(SystemStats stats) const {
+//}
---
+++ src/print_opts.h
@@ -0,0 +1,34 @@
+#ifndef PRINT_OPTS_H
+#define PRINT_OPTS_H
+
+#include "event.h"
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+
+
+class Print_opts {
+public:
+	Print_opts(bool verbose, bool per_thread) {
+		this->verbose = verbose;
+		this->per_thread = per_thread;
+	}
+
+	void print_state_transition(const Event* event, Thread::State before_state, Thread::State after_state);
+
+	void print_dispatch_invoked_message(const Event *event, Thread *thread, std::string message);
+
+	void print_process_details(Process* process) const;
+
+	//void print_statistics(SystemStats stats) const;
+
+private:
+
+	bool verbose;
+
+	bool per_thread;
+
+	bool color;
+};
+
+#endif
