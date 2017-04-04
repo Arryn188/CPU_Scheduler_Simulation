a5ff4200661f576103bb639f83ee122025b8fb42
RebeccaMay <remay@mines.edu>
2017-04-04 10:43:05 -0600

    priority done but not tested

--- src/priorityscheduler.cpp
+++ src/priorityscheduler.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "priorityscheduler.h"
+using namespace std;
+
+Decision* PriorityScheduler::get_next_thread(const Event* event){
+    Thread *nextT = NULL;
+    int i = 0;
+
+    for (i = 0; i < 4; i++){
+        if(!threads[i].empty()){
+            nextT = threads[i].front();
+            threads[i].pop();
+            break;
+        }
+    }
+
+    char reason[250];
+    sprintf(reason, "Selected from %s queue with %u threads; will run until completion of burst.", nextT ? Process::get_type_name(nextT-> process -> get_type()).c_str() : "", (unsigned int) threads[i].size() + 1);
+    string explain = string(reason);
+
+    Decision *decision = new Decision(nextT, -1, explain);
+    return decision;
+}
+
+void PriorityScheduler::enqueue(const Event* event, Thread* thread){
+    int prior = thread->process->get_type();
+    threads[prior].push(thread);
+}
--- src/priorityscheduler.h
+++ src/priorityscheduler.h
@@ -12,7 +12,8 @@ class PriorityScheduler: public Scheduler{
 public:
     Decision* get_next_thread(const Event* event);
     void enqueue(const Event * event, Thread* thread);
+private:
+    queue<Thread*> threads[4] = {queue<Thread*>(), queue<Thread*>(), queue<Thread*>(), queue<Thread*>()};
 };
-
 
 #endif //SCHED_PRIORITYSCHEDULER_H
